995c26d211fdcf55a938b12419ea30885bc4b5d0
sasotomayor <sasotomayor@uc.cl>
2020-04-27 13:25:33 -0400

    postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_27_003814) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "burgers", force: :cascade do |t|
     t.string "nombre"
     t.integer "precio"
