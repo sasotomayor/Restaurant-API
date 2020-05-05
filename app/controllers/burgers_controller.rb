b62f6d0edb0b8836f1dc40ee512176d8c40b2d39
sasotomayor <sasotomayor@uc.cl>
2020-05-04 22:11:33 -0400

    a

--- app/controllers/burgers_controller.rb
+++ app/controllers/burgers_controller.rb
@@ -1,7 +1,7 @@
 class BurgersController < ApplicationController
   def index
     burgers = Burger.order('id ASC');
-    render json: burgers.map{|burger| {id: burger[:id], nombre: burger[:nombre], descripcion: burger[:descripcion], imagen: burger[:imagen],
+    render json: burgers.map{|burger| {id: burger[:id], nombre: burger[:nombre], descripcion: burger[:descripcion], precio: burger[:precio], imagen: burger[:imagen],
       ingredientes: burger.ingredients.map{|ing| {path: "https://whispering-thicket-50827.herokuapp.com/ingrediente/#{ing[:id]}"}}}}, status: 200
   end
 
@@ -11,7 +11,7 @@ class BurgersController < ApplicationController
         render json: {message: 'Hamburguesa inexistente'}, status: 404
       else
         burger = Burger.find(params[:id])
-        render json: {id: burger[:id], nombre: burger[:nombre], descripcion: burger[:descripcion], imagen: burger[:imagen],
+        render json: {id: burger[:id], nombre: burger[:nombre], descripcion: burger[:descripcion], precio: burger[:precio], imagen: burger[:imagen],
           ingredientes: burger.ingredients.map{|ing| {path: "https://whispering-thicket-50827.herokuapp.com/ingrediente/#{ing[:id]}"}}}, status: 200
       end
     else
@@ -23,7 +23,7 @@ class BurgersController < ApplicationController
     burger = Burger.new(burger_params)
     if burger_params[:nombre] && burger_params[:descripcion] && burger_params[:imagen] && burger_params[:precio]
       if burger.save
-        render json: {id: burger[:id], nombre: burger[:nombre], descripcion: burger[:descripcion], imagen: burger[:imagen],
+        render json: {id: burger[:id], nombre: burger[:nombre], descripcion: burger[:descripcion], precio: burger[:precio], imagen: burger[:imagen],
           ingredientes: burger.ingredients.map{|ing| {path: "https://whispering-thicket-50827.herokuapp.com/ingrediente/#{ing[:id]}"}}}, status: 201
       else
         render json: {message: 'Input inválido'}, status: 400
